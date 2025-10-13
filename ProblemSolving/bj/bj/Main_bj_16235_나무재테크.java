8f4eaf827b7c834b631130ef7e8582ce01e14f23
kimhiwon <kimhiwon@naver.com>
2025-10-13 09:09:14 +0900

    PS: swea 4012

---
+++ ProblemSolving/bj/bj/Main_bj_16235_나무재테크.java
@@ -0,0 +1,13 @@
+import java.io.*; 
+import java.util.*; 
+public class Main_bj_16235_나무재테크 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null; 
+		
+		
+		
+	}
+
+}
