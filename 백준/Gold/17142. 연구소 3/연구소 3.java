e2ba05a01df22f58d56ad4c79b2aa1b51889ba63
Heewon Kim <75614782+heewon916@users.noreply.github.com>
2025-10-20 23:17:41 +0900

    [Gold III] Title: 연구소 3, Time: 192 ms, Memory: 27212 KB -BaekjoonHub

---
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -0,0 +1,162 @@
+import java.io.*; 
+import java.util.*; 
+public class Main {
+	static int[][] map; 
+	static int N, M; 
+	static boolean[][] done; 
+	static ArrayList<Virus> list; 
+	static int[] b; 
+	static int emptyCount = 0; 
+	static int minTime; 
+	static int[] di = {-1, 1, 0, 0};
+	static int[] dj = {0, 0, -1, 1}; 
+	
+	static class Virus{
+		int r, c, time; 
+		Virus(int r, int c, int time){
+			this.r = r; 
+			this.c = c;
+			this.time = time; 
+		}
+	}
+	
+	static void input(BufferedReader br, StringTokenizer st) throws IOException {
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		minTime = Integer.MAX_VALUE; 
+		map = new int[N][N]; 
+		
+		list = new ArrayList<>(); // 바이러스의 위치 리스트 
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 2) list.add(new Virus(i, j, 0));
+				else if(map[i][j] == 0) emptyCount++; 
+			}
+		}
+	}
+	
+	static void comb(int cnt, int start) {
+		if(cnt == M) {
+			// System.out.println(Arrays.toString(b));
+			ArrayList<Virus> virus = new ArrayList<>(); 
+			for(int i=0; i<b.length; i++) {
+				if(b[i] == 1) virus.add(list.get(i));
+			}
+			bfs(virus);
+			return; 
+		}
+		for(int i=start; i<list.size(); i++) {
+			b[i] = 1; 
+			comb(cnt+1, i+1);
+			b[i] = 0; 
+		}
+	}
+	
+	/**
+	 * 문제 처음에 잘못 이해함 아놔;; 
+	 * 언제 다 방문하는 걸로 계산해서 어쩔 땐 답이 맞고 틀렸는데, 
+	 * 그게 아니라 언제 다 전염되느냐의 최소 시간이기 때문에 
+	 * 그걸 고쳐야 했음ㅠㅠ
+	 * 다시 풀어 이거.. 하
+	 * @param virus
+	 */
+	static void bfs(ArrayList<Virus> virus) {
+		ArrayDeque<Virus> dq = new ArrayDeque<>(); 
+		done = new boolean[N][N]; // 활성화된 바이러스를 표시한다 
+		
+		/**
+		 * 일단 초기 활성 바이러스 모두 방문처리하고 
+		 */
+		for(Virus v: virus) {
+			dq.offer(v); 
+			done[v.r][v.c] = true;  
+		}
+		Virus v = null;
+		int maxEmptyTime = 0; 
+		while(!dq.isEmpty()) {
+			v = dq.poll();
+			done[v.r][v.c] = true; 
+			
+			for(int d=0; d<4; d++) {
+				int ni = v.r + di[d]; 
+				int nj = v.c + dj[d]; 
+				if(ni<0 || ni>=N || nj<0 || nj>= N) continue; 
+				/**
+				 * 활성 바이러스가 비활성 바이러스가 있는 칸으로 가면 
+				 * 비활성 바이러스가 활성으로 변한다. 
+				 */
+				if(!done[ni][nj] && (map[ni][nj] == 0 || map[ni][nj] == 2)) {
+					dq.offer(new Virus(ni, nj, v.time+1));
+					done[ni][nj] = true; 
+					
+					/**
+					 * 만약 지금 감염시킨 곳이 '빈칸'이면
+					 * 그 감염 시간을 maxEmptyTime에 갱신한다. 
+					 * => 모두 방문하는 게 목적이 아니라, 언제 다 전염되는지가 중요한거라서 
+					 */
+					if(map[ni][nj] == 0) {
+						maxEmptyTime = Math.max(maxEmptyTime, v.time+1);
+					}
+				}
+			}
+		}
+		if(checkAll(done)) {
+			minTime = Math.min(minTime, maxEmptyTime);
+			return; 
+		}
+		
+	}
+
+	static boolean checkAll(boolean[][] v) {
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(!v[i][j] && map[i][j] == 0) return false; 
+			}
+		}
+		return true; 
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null; 
+		
+		input(br, st);
+		
+		if(emptyCount == 0) {
+			System.out.println(0);
+			return; 
+		}
+		b = new int[list.size()]; 
+		comb(0, 0);
+		if(minTime == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		}else System.out.println(minTime);
+	}
+
+}
+/*
+ㅁㅊ 반례;; 
+3 1
+2 2 0
+2 1 1
+2 2 0
+=> 3
+-----
+NxN 배열이 있고 50 x 50 
+빈 칸 0 
+벽 1
+바이러스 2 
+1<= M <= 비활성 바이러스 개수 <= 10 
+M개만 활성상태로 만들었을 때 모든 빈칸에 바이러스가 퍼지는 최소시간 
+(10C1 + 10C2 + 10C3 + 10C4) * 2 + 10C5 의 조합이 있겠지 
+근데 M을 최소한으로 하라곤 안 했으니까 그냥 M개 최대한 바이러스를 많이 놓으면 도움이 될 것 같은데 
+2의 위치 중 어디에 놓을 것인가 
+
+바이러스가 있는 좌표를 갖고 온다 
+그 좌표들에 대해서 M개를 선택한다
+그 자리에 각각 활성화시킨다. 
+	상하좌우 모든 빈 칸으로 동시에 복제된다 
+
+*/
