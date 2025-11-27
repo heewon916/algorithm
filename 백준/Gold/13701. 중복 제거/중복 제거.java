9b1876e32d74fd65f3088cfdb7812ee11cd370e6
Heewon Kim <75614782+heewon916@users.noreply.github.com>
2025-11-27 23:54:22 +0900

    [Gold IV] Title: 중복 제거, Time: 2448 ms, Memory: 516324 KB -BaekjoonHub

---
+++ 백준/Gold/13701. 중복 제거/중복 제거.java
@@ -0,0 +1,25 @@
+import java.io.*; 
+import java.util.*; 
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null; 
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		Map<Integer, Integer> map = new HashMap<>(); 
+		StringBuilder sb = new StringBuilder(); 
+		while(st.hasMoreTokens()) {
+			int n = Integer.parseInt(st.nextToken());
+			
+			if(map.containsKey(n)) continue; 
+			else {
+				sb.append(n + " ");
+				map.put(n, 1);
+			}
+		}
+		System.out.println(sb);
+
+	}
+
+}
