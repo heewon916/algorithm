0a42d3b15c7f48262425d94527a505d6ff791749
kimhiwon <kimhiwon@naver.com>
2025-12-01 21:40:00 +0900

    PS; bj

---
+++ ProblemSolving/bj/Main_bj_2169_로봇조종하기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Main_bj_2169_로봇조종하기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+    }
+}
