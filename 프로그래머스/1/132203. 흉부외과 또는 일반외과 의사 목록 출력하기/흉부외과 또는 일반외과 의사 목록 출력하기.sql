1e9acc428f6cb140ddf37d73e910859bd743c6ca
Heewon Kim <75614782+heewon916@users.noreply.github.com>
2025-12-02 11:34:14 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,8 +1,4 @@
--- dr_name, dr_id, lcns_id, hire_ymd, mcdp_cd, tlno 
--- 진료과가 CS이거나 GS인 
--- 의사이름, 의사id, 진료과, 고용일자 
--- 고용일자 기준 내림차순, 이름 기준 오름차순 정렬 
-SELECT dr_name, dr_id, mcdp_cd, date_format(hire_ymd, '%Y-%m-%d')
-from doctor  
-where mcdp_cd = 'cs' or mcdp_cd = 'gs'
+select dr_name, dr_id, mcdp_cd, date_format(hire_ymd, "%Y-%m-%d") as hire_ymd 
+from doctor 
+where mcdp_cd = 'CS' or mcdp_cd = 'GS'
 order by hire_ymd desc, dr_name asc; 
