d698df796b854572905f3e0c8eb117a1ace6ca34
kimhiwon <kimhiwon@naver.com>
2025-12-03 09:59:30 +0900

    update: bj 23757 시간 복잡도 설명 추가

---
+++ ProblemSolving/bj/bj/Main_bj_25418_정수a를k로만들기.java
@@ -0,0 +1,15 @@
+package bj;
+import java.util.*;
+import java.io.*;
+public class Main_bj_25418_정수a를k로만들기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+
+    }
+}
+/*
+
+
+ */
