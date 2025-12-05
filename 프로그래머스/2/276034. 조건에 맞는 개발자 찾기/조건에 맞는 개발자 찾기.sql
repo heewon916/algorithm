f659a2d7a6d89102b755748a170389db2eb8195d
Heewon Kim <75614782+heewon916@users.noreply.github.com>
2025-12-05 13:58:19 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,12 +1,4 @@
--- skillcodes 테이블: name, category, code 
--- developers 테이블: id, first_name, last_name, email, skill_code 
--- python 또는 c# 스킬을 가진 
--- 개발자의 id, 이메일, 이름, 성을 조회 
--- id 기준으로 오름차순 정렬 
-select d.id, d.email, d.first_name, d.last_name 
-from developers d 
-join (select sum(sc.code) as mask 
-     from skillcodes sc
-     where name in ('Python', 'C#')) as s 
-where (d.skill_code & s.mask) > 0
-order by d.id;
+select id, email, first_name, last_name 
+from developers d join (select sum(code) as sumCode from skillcodes where name="Python" or name="C#") as s
+where d.skill_code & s.sumCode > 0 
+order by id asc; 
