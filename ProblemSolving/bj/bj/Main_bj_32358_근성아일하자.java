395a6bbdc4c817ca8ab024a09759144f3248eefb
kimhiwon <kimhiwon@naver.com>
2026-01-06 17:16:26 +0900

    fix: bj 32358

--- ProblemSolving/bj/bj/Main_bj_32358_근성아일하자.java
+++ ProblemSolving/bj/bj/Main_bj_32358_근성아일하자.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 public class Main_bj_32358_근성아일하자 {
     static int N;
-    static PriorityQueue<Integer> pq; // 좌표, 차이
-    static int start;
+    static TreeMap<Integer, Integer> map; 
+    static Integer start;
     static int totalMove;
 
     public static void main(String[] args) throws Exception {
@@ -17,45 +17,66 @@ public class Main_bj_32358_근성아일하자 {
         N = Integer.parseInt(st.nextToken());
 
         start = 0;
-        pq = new PriorityQueue<>((o1, o2) -> {
-            if (Math.abs(start - o1) == Math.abs(start - o2)) {
-                return o1 - o2; // 더 작은 걸 리턴
-            } else {
-                return Math.abs(start - o1) - Math.abs(start - o2);
-            }
-        });
-
+        map = new TreeMap<>();
+        totalMove = 0; 
+        int trashCnt = 0; 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int cmd = -1, pos = 0;
             if (st.countTokens() == 2) {
                 cmd = Integer.parseInt(st.nextToken());
                 pos = Integer.parseInt(st.nextToken());
-                // System.out.println("cmd: " + cmd + ", " + pos);
             } else if (st.countTokens() == 1) {
                 cmd = Integer.parseInt(st.nextToken());
-                // System.out.println("cmd: " + cmd);
             }
 
             if (cmd == 1) {
-                pq.add(pos);
+                if(map.containsKey(pos)){
+                    map.put(pos, map.get(pos)+1);
+                }else{
+                    map.put(pos, 1);
+                }
+                trashCnt++; 
             } else {
-                while (!pq.isEmpty()) {
-                    int newPos = pq.poll();
-                    totalMove += Math.abs(newPos - start);
-                    start = newPos;
-                    PriorityQueue<Integer> temp = new PriorityQueue<>((o1, o2) -> {
-                        if (Math.abs(start - o1) == Math.abs(start - o2)) {
-                            return o1 - o2; // 더 작은 걸 리턴
-                        } else {
-                            return Math.abs(start - o1) - Math.abs(start - o2);
+                while(trashCnt > 0){
+                    Integer left = map.floorKey(start);
+                    Integer right = map.ceilingKey(start);
+                    Integer target = null; 
+
+                    if(left == null){
+                        target = right; 
+                    }else if(right == null){
+                        target = left; 
+                    }else{
+                        // 둘 다 있으면 거리 차이 가져오고 
+                        int ldiff = Math.abs(start - left);
+                        int rdiff = Math.abs(start - right);
+    
+                        // 왼쪽이 더 가까우면 
+                        if(ldiff < rdiff) {
+                            target = left; 
+                        }else if(ldiff > rdiff){ 
+                            // 오른쪽이 더 가까우면 
+                            target = right; 
+                        }else{
+                            // 둘이 거리가 같으면 더 작은 좌표를 타겟으로 가져감 
+                            if(left <= right){
+                                target = left; 
+                            }else{
+                                target = right; 
+                            }
                         }
-                    });
-                    for(int a: pq){
-                        temp.add(a); 
+                        // 이동거리 축적하고 현재 위치 target으로 변경 
+                        totalMove += Math.abs(start - target);
+                        start = target; 
+                        // 만약 target 좌표에 있던 쓰레기 개수가 0이 되면 그 좌표는 아예 delete
+                        if(map.get(target) == 0){
+                            map.remove(target);
+                        }else{
+                            map.put(target, map.get(target)-1);
+                        }
                     }
-                    pq = temp; 
-                    // System.out.println("새로운 이동위치: " + newPos + ", 총 이동거리 : " + totalMove);
+                    trashCnt--; 
                 }
             }
 
