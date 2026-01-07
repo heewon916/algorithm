d7c5c7094f79ae7c920a4ffd6854db6a220cfac8
kimhiwon <kimhiwon@naver.com>
2026-01-07 17:02:51 +0900

    fix: bj 13703

--- ProblemSolving/bj/bj/Main_bj_13703_물벼룩의생존확률.java
+++ ProblemSolving/bj/bj/Main_bj_13703_물벼룩의생존확률.java
@@ -1,4 +1,12 @@
-class Main {
+package bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main_bj_13703_물벼룩의생존확률 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
 
