36a510e7bc6e2d224021d6fb54f2d2d0390223a1
Heewon Kim <75614782+heewon916@users.noreply.github.com>
2026-04-11 23:19:05 +0900

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.05 ms
+메모리: 4.21 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 02일 00:08:27
+2026년 04월 11일 23:19:02
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,63 +1,48 @@
 #include <string>
 #include <vector>
-#include <iostream> 
-#include <unordered_map> 
-#include <algorithm>
+#include <algorithm> 
+
 using namespace std;
-struct Song{
-    int songNo; 
-    int playCnt; 
-};
-// vector: 단일 요소만을 이어붙인다. 
-// vector<string, int> 이런 구조는 없다 
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
-    unordered_map<string, int> genreMap;
-    unordered_map<string, vector<Song>> playMap; 
+    // 1. 장르별 총 재생 횟수 구하기 
+    unordered_map<string, int> hmap; 
+    // 2. 장르별 {재생횟수, 고유번호} 순으로 리스트 관리 
+    unordered_map<string, vector<pair<int, int>>> playMap;
     for(int i=0; i<genres.size(); i++){
-        genreMap[genres[i]] += plays[i]; 
-        playMap[genres[i]].push_back({i, plays[i]}); // 구조체 형태로 삽입해야 한다. {} 
+        hmap[genres[i]] += plays[i]; 
+        playMap[genres[i]].push_back({plays[i], i});
     }
     
-    // map은 정렬이 불가능하다 순서 개념 자체가 없기 때문이다 
-    // 따라서 map으로 만든 데이터를 정렬하고 싶으면 vector로 바꿔서 진행해줘야 한다 
-    // vector: ArrayList/ pair: <?, ?> 딱 2개만 담을 수 있는 배열 -> 그래서 first, second 로 접근한다 
-    vector<pair<string, int>> genreList(genreMap.begin(), genreMap.end());
-    
-    sort(genreList.begin(), genreList.end(), [](pair<string, int> a, pair<string, int> b){
-        return a.second > b.second; 
+    // 3. 장르 순서 구하기 
+    vector<pair<string, int>> sortHmap(hmap.begin(), hmap.end()); 
+    sort(sortHmap.begin(), sortHmap.end(), [](const auto& a, const auto& b){
+        if(a.second != b.second) return a.second > b.second; // 내림차순 
+        return a.first < b.first; 
     });
     
-    vector<int> answer; 
-    // & 얘를 붙이면 위치 복사라서 무지성 복사가 안 일어난다고 함 
-    for(const auto& pair: genreList){
-        string genre = pair.first; 
-        vector<Song> songs = playMap[genre];
-        
-        // Song을 이제 정렬해야 함 
-        sort(songs.begin(), songs.end(), [](Song& a, Song& b){
-           if(a.playCnt == b.playCnt){
-               return a.songNo < b.songNo; // 고유 번호는 오름차순 정렬 
-            }
-            return a.playCnt > b.playCnt; // 재생 횟수는 내림차순 정렬 
+    // 4. 장르별 곡 안에 정렬하기 
+    for(auto& entry: playMap){
+        auto& a = entry.second; 
+        sort(a.begin(), a.end() ,[](const auto& a, const auto& b){
+            if(a.first != b.second) return a.first > b.first; // 재생횟수는 내림차순
+            else return a.second < b.second; // 고유번호는 오름차순 
         });
+    }
         
-        int cnt = 0; 
-        for(Song& s: songs){
-            answer.push_back(s.songNo); 
-            cnt++; 
-            if(cnt == 2) break; 
+    // 5. 장르별로 2곡씩 땡겨오기 
+    vector<int> answer;
+    for(auto entry: sortHmap){
+        string genre = entry.first;
+        vector<pair<int, int>> temp = playMap[genre]; 
+        int len = (2 < temp.size())? 2: temp.size();  // 2곡만 담아야 함 
+        for(int i=0; i<len; i++){
+            answer.push_back(temp[i].second); 
         }
     }
-    
-    return answer; 
-    
-    
+    return answer;
 }
-
-/**
-나열 순서: 장르의 총 재생횟수 내림차순 > 장르 내 노래별 재생 횟수 내림차순 > 고유 번호 오름차순 
-
-1. 장르: 총 재생횟수
-2. 장르별로 노래들 -> 재생횟수로 정렬 장르: <노래, 횟수> -> 횟수로 내림차순 > 고유 번호 오름차순
+/*
+장르: 총 재생횟수 -> 
+장르: {재생횟수, 고유번호}
 */
